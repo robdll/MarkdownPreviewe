80a53ca6980a244b714f00215f64721ebf2095bb
roberto di lillo <dilillo.roberto@gmail.com>
2022-05-22 04:46:13 +0100

    style fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,42 @@ function App() {
     gfm: true
   });
   
-  const initialState = "# Welcome to my React Markdown Previewer!"
+  const initialState = `# Example Title
+## Example Subtitle
+
+### Example Heading
+
+Example text in **bold** **italic** and ~~Strikethrough~~
+
+Example blockQuote
+> The way to love anything is to realize that it may be lost.
+
+Example [Link](https://robertodilillo.dev)
+
+here is some inline code: \`console.log('Hello Wordl')\`
+
+here is a code block:
+\`\`\`
+function welcome() {
+  var myWelcome = 'Hello Wordl';
+  console.log(myWelcome);
+}
+\`\`\`
+
+Here is a bullet list:
+- With different
+  - indentation
+     - level
+
+Here is a numbered list
+  1. With repeated
+  1. number
+
+And an image:
+
+![Logo Rob.dll](https://www.robertodilillo.dev/resources/robdll-logo.svg)
+
+`
   const [ mdText, setMdText] = useState(initialState)
 
   const handleChange = (event) => {
